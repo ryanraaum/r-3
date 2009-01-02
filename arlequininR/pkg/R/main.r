f825b6da79bd63af690bb752a3c71f51e65a681e
Ryan Raaum <code@raaum.org>
2009-01-02 13:34:52 -0500

    adding arlequininR stuff

--- arlequininR/pkg/R/main.r
+++ arlequininR/pkg/R/main.r
@@ -85,3 +85,8 @@ function (filename)
 	return(gd)
 }
 
+"conv" <-
+function(a, b) 
+{
+  .C("convolve", as.double(a), as.integer(length(a)), as.double(b), as.integer(length(b)), ab = double(length(a) + length(b) - 1))$ab
+}
--- arlequininR/pkg/src/arlequin.c
+++ arlequininR/pkg/src/arlequin.c
@@ -2,6 +2,17 @@
 #include "arlequin.h"
 
 
+void convolve(double *a, int *na, double *b, int *nb, double *ab)
+     {
+       int i, j, nab = *na + *nb - 1;
+     
+       for(i = 0; i < nab; i++)
+         ab[i] = 0.0;
+       for(i = 0; i < *na; i++)
+         for(j = 0; j < *nb; j++)
+           ab[i + j] += a[i] * b[j];
+     }
+
 int read_arlequin_file( const char *filename, 
                         int *ploidy,
                         char **genotypes, 
